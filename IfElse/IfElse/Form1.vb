db9e6541479276e6d8fb87e2760e18f54635b8a7
idsc3101 <idsc3101@gmail.com>
2014-09-22 17:15:16 -0500

    solved IfElse exercise on 9/22/14

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -5,26 +5,47 @@ Public Class Form1
     Private Sub btnFindLarger_Click(sender As System.Object, e As System.EventArgs) Handles btnFindLarger.Click
         Dim num1, num2, largerNum As Double
 
-        ' Modify the program such that it does not crash 
-        ' if the user types incorrect input
-
+        '1
         Dim st1 As String = txtFirstNum.Text
         Dim st2 As String = txtSecondNum.Text
-
-        num1 = CDbl(st1)
-        num2 = CDbl(st2)
-
-        Dim result As String
-
-        If num1 > num2 Then
-            result = "The larger number is " & num1
-        ElseIf num2 > num1 Then
-            result = "The larger number is " & num2
-        Else
-            result = "They are equal"
+        '2
+        'Checkpoint
+        If Not (IsNumeric(st1) And IsNumeric(st2)) Then
+            MessageBox.Show("Please, enter numbers!")
+            Return
         End If
 
-        txtResult.Text = result
+        '3
+        'Clean area
+
+   
+        num1 = CDbl(st1)
+        num2 = CDbl(st2)
+        '4
+
+        'Checkpoint
+        If Not (num1 >= 0 And num2 >= 0) Then
+            MessageBox.Show("Please, enter non-negative numbers!")
+            Return
+        End If
+
+            Dim result As String
+
+            If num1 > num2 Then
+                result = "The larger number is " & num1
+            ElseIf num2 > num1 Then
+                result = "The larger number is " & num2
+            Else
+                result = "They are equal"
+            End If
+
+
+            txtResult.Text = result
+
+
+
+
+
     End Sub
 
 End Class
